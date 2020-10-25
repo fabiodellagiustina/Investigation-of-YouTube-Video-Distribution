8dcfe8f93e8982842d20031b5ad17e10cd58b13b
Fabio <fabiodellagiustina1997@gmail.com>
2020-10-25 11:22:02 +0200

    Update crontab mumbai

--- provision_vantage_point.sh
+++ provision_vantage_point.sh
@@ -4,3 +4,4 @@ sudo apt-get update
 sudo apt-get install -y python
 git clone https://github.com/fabiodellagiustina/Investigation-of-YouTube-Video-Distribution.git
 crontab Investigation-of-YouTube-Video-Distribution/crontab
+# crontab Investigation-of-YouTube-Video-Distribution/crontab_mumbai

1ae74f058ede2a645d5a94029582a88423236885
Fabio <fabiodellagiustina1997@gmail.com>
2020-10-25 12:15:31 +0200

    Update BATCH_MODE = True

--- lib_pytomo/pytomo/config_pytomo.py
+++ lib_pytomo/pytomo/config_pytomo.py
@@ -13,7 +13,7 @@ USE_PACKAGE_DIR = False
 
 # execute crawl without prompting user for any parameters
 # put to True if you are running jobs automatically
-BATCH_MODE = False
+BATCH_MODE = True
 
 #PROVIDER = None
 PROVIDER = ''

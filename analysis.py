16120c2f31db8ac96f9a8676fcf19a249263d74d
aabriani <44431611+aabriani@users.noreply.github.com>
2020-10-30 02:14:53 +0200

    update analysis.py

--- analysis.py
+++ analysis.py
@@ -8,9 +8,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 # from zipfile import ZipFile
 import sqlite3
+import itertools
 
 DB_COLUMNS = ['ID', 'Url', 'CacheUrl', 'CacheServerDelay', 'IP', 'ASNumber', 'PingMin', 'TimeToGetFirstByte', 'RedirectUrl', 'StatusCode']
 VANTAGE_POINT_HOSTNAME_LIST = {'us-west-1': 'ip-172-31-14-163', 'us-east-1': 'ip-172-31-85-175', 'eu-central-1': 'ip-172-31-43-135', 'ap-south-1': 'ip-172-31-13-30', 'ap-northeast-1': 'ip-172-31-4-22'}
+URL_UPLOADED_VIDEO = 'https://youtu.be/GDJs8iMzNSc'
 
 def retrieve_database(db):
     conn = sqlite3.connect(db)
@@ -72,3 +74,82 @@ print(df_ap_south_1)
 print(df_ap_northeast_1)
 
 # sanitize all vantage point general dataframes
+# df_us_west_1
+df_us_west_1.dropna(subset=['StatusCode'], inplace=True)
+drop_index = df_us_west_1[(df_us_west_1['TimeToGetFirstByte'] == np.nan) & (df_us_west_1['StatusCode'] == 200.0)].index
+print(drop_index)
+df_us_west_1.drop(drop_index , inplace=True)
+# df_us_east_1
+df_us_east_1.dropna(subset=['StatusCode'], inplace=True)
+drop_index = df_us_east_1[(df_us_east_1['TimeToGetFirstByte'] == np.nan) & (df_us_east_1['StatusCode'] == 200.0)].index
+print(drop_index)
+df_us_east_1.drop(drop_index , inplace=True)
+# df_eu_central_1
+df_eu_central_1.dropna(subset=['StatusCode'], inplace=True)
+drop_index = df_eu_central_1[(df_eu_central_1['TimeToGetFirstByte'] == np.nan) & (df_eu_central_1['StatusCode'] == 200.0)].index
+print(drop_index)
+df_eu_central_1.drop(drop_index , inplace=True)
+# df_ap_south_1
+df_ap_south_1.dropna(subset=['StatusCode'], inplace=True)
+drop_index = df_ap_south_1[(df_ap_south_1['TimeToGetFirstByte'] == np.nan) & (df_ap_south_1['StatusCode'] == 200.0)].index
+print(drop_index)
+df_ap_south_1.drop(drop_index , inplace=True)
+# df_ap_northeast_1
+df_ap_northeast_1.dropna(subset=['StatusCode'], inplace=True)
+drop_index = df_ap_northeast_1[(df_ap_northeast_1['TimeToGetFirstByte'] == np.nan) & (df_ap_northeast_1['StatusCode'] == 200.0)].index
+print(drop_index)
+df_ap_northeast_1.drop(drop_index , inplace=True)
+
+print(df_us_west_1)
+print(df_us_east_1)
+print(df_eu_central_1)
+print(df_ap_south_1)
+print(df_ap_northeast_1)
+
+# extract records for uploaded video and mark vantage point
+vantage_point = []
+columns_uploaded = DB_COLUMNS
+df_uploaded = pd.DataFrame(columns=DB_COLUMNS)
+# df_uploaded = pd.DataFrame(columns=list(itertools.chain(DB_COLUMNS, ['VantagePoint']))
+# df_us_west_1
+subdf = df_us_west_1[df_us_west_1['Url'] == URL_UPLOADED_VIDEO]
+print(subdf)
+print(len(subdf.index))
+vantage_point.extend([VANTAGE_POINT_HOSTNAME_LIST['us-west-1']]*len(subdf.index))
+df_us_west_1.drop(subdf.index, inplace=True)
+df_uploaded = pd.concat([df_uploaded, subdf], axis=0, ignore_index=True)
+# df_us_east_1
+subdf = df_us_east_1[df_us_east_1['Url'] == URL_UPLOADED_VIDEO]
+print(subdf)
+print(len(subdf.index))
+vantage_point.extend([VANTAGE_POINT_HOSTNAME_LIST['us-east-1']]*len(subdf.index))
+df_us_east_1.drop(subdf.index, inplace=True)
+df_uploaded = pd.concat([df_uploaded, subdf], axis=0, ignore_index=True)
+# df_eu_central_1
+subdf = df_eu_central_1[df_eu_central_1['Url'] == URL_UPLOADED_VIDEO]
+print(subdf)
+print(len(subdf.index))
+vantage_point.extend([VANTAGE_POINT_HOSTNAME_LIST['eu-central-1']]*len(subdf.index))
+df_eu_central_1.drop(subdf.index, inplace=True)
+df_uploaded = pd.concat([df_uploaded, subdf], axis=0, ignore_index=True)
+# df_ap_south_1
+subdf = df_ap_south_1[df_ap_south_1['Url'] == URL_UPLOADED_VIDEO]
+print(subdf)
+print(len(subdf.index))
+vantage_point.extend([VANTAGE_POINT_HOSTNAME_LIST['ap-south-1']]*len(subdf.index))
+df_ap_south_1.drop(subdf.index, inplace=True)
+df_uploaded = pd.concat([df_uploaded, subdf], axis=0, ignore_index=True)
+# df_ap_south_1
+subdf = df_ap_northeast_1[df_ap_northeast_1['Url'] == URL_UPLOADED_VIDEO]
+print(subdf)
+print(len(subdf.index))
+vantage_point.extend([VANTAGE_POINT_HOSTNAME_LIST['ap-northeast-1']]*len(subdf.index))
+print(df_ap_northeast_1)
+df_ap_northeast_1.drop(subdf.index, inplace=True)
+print(df_ap_northeast_1)
+df_uploaded = pd.concat([df_uploaded, subdf], axis=0, ignore_index=True)
+
+print(df_uploaded)
+print(vantage_point)
+df_uploaded['VantagePoint'] = vantage_point
+print(df_uploaded)

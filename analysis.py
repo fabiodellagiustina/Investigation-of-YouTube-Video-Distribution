7debd80ff1602fbada128caeb2c51c1637a4f621
aabriani <44431611+aabriani@users.noreply.github.com>
2020-10-31 23:31:58 +0200

    added ip similarities lookup, cumulative redirect, plot for each v_point

--- analysis.py
+++ analysis.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 # from zipfile import ZipFile
 import sqlite3
 import itertools
+import datetime
 
 DB_COLUMNS = ['ID', 'Url', 'CacheUrl', 'CacheServerDelay', 'IP', 'ASNumber', 'PingMin', 'TimeTogetFirstByte', 'RedirectUrl', 'StatusCode']
 VANTAGE_POINT_HOSTNAME_LIST = {'us-west-1': 'ip-172-31-14-163', 'us-east-1': 'ip-172-31-85-175', 'eu-central-1': 'ip-172-31-43-135', 'ap-south-1': 'ip-172-31-13-30', 'ap-northeast-1': 'ip-172-31-4-22'}
@@ -71,6 +72,113 @@ def hops_statistics(df_list, cacheurl_list):
                 print("Error in extraction of hops statistics for Timestamp: %s" %(str(row['ID'])))
     return [cacheurl_dict_firsthop, cacheurl_dict_lasthop, cacheurl_dict_nredirects]
 
+def divide_runs(g):
+    col = list(g.columns)
+    g1 = pd.DataFrame(columns=col)
+    g2 = pd.DataFrame(columns=col)
+    for index, row in g.iterrows():
+        dt = datetime.datetime.strptime(row['ID'], '%Y-%m-%d %H:%M:%S.%f')
+        if dt.day in [27, 28]:
+            g1 = g1.append(row, ignore_index=True)
+        elif dt.day in [29, 30]:
+            g2 = g2.append(row, ignore_index=True)
+    print(g1)
+    print(g2)
+    return [g1, g2]
+
+def cumulative_redirect_stats(name, group, popular_video_list, not_popular_video_list):
+    hours_list = [12, 18, 0, 6]
+    print(popular_video_list)
+    print(group)
+    print(group['Url'])
+    print(group['Url'].isin(popular_video_list))
+    filt_pop_group = group[group['Url'].isin(popular_video_list)]
+    filt_not_pop_group = group[group['Url'].isin(not_popular_video_list)]
+    print(filt_pop_group)
+    print(filt_not_pop_group)
+    cacheurl_pop_nredirects = dict.fromkeys(hours_list, 0)
+    cacheurl_not_pop_nredirects = dict.fromkeys(hours_list, 0)
+    dt_logger = []
+    dt_h_previous = -1
+    for index, row in filt_pop_group.iterrows():
+        dt = datetime.datetime.strptime(row['ID'], '%Y-%m-%d %H:%M:%S.%f')
+        cacheurl_pop_nredirects[dt.hour] += len(row['CacheUrl'])
+        # save a sample datetime
+        if dt.hour != dt_h_previous:
+            dt_h_previous = dt.hour
+            dt_logger.append(dt.strftime("%Y-%m-%d %H:00"))
+    for index, row in filt_not_pop_group.iterrows():
+        dt = datetime.datetime.strptime(row['ID'], '%Y-%m-%d %H:%M:%S.%f')
+        cacheurl_not_pop_nredirects[dt.hour] += len(row['CacheUrl'])
+        # if (type(row['CacheUrl']) == list) and (len(row['CacheUrl']) == 1):
+    print(cacheurl_pop_nredirects)
+    print(cacheurl_not_pop_nredirects)
+    list_of_values = list(zip(cacheurl_pop_nredirects.keys(), cacheurl_pop_nredirects.values(), cacheurl_not_pop_nredirects.values(), dt_logger))
+    temp_df = pd.DataFrame(list_of_values, columns=['Hour', 'CumulRedirectsPopular', 'CumulRedirectsNotPopular', 'DateTime'])
+    temp_df['Hour'] = temp_df['Hour'].astype(int)
+    # temp_df.sort_values('Hour', inplace=True)
+    print(temp_df)
+    return temp_df
+
+def plot_cumulative_redirects(df, name, r):
+    ax = plt.gca()
+    x_ticks = np.arange(0, 4, 1)
+    plt.xticks(x_ticks)
+    # labels = ['12', '18', '0', '6']
+    # ax.set_xticklabels(labels)
+    # ax.locator_params(axis='y', nbins=4)
+    ax.set_xticklabels(df.DateTime, rotation=90)
+    df.plot(y='CumulRedirectsPopular', use_index=True, kind = 'line', ax=ax, label='Popular videos')
+    df.plot(y='CumulRedirectsNotPopular', use_index=True, kind = 'line', color='red', ax=ax, label='Non-popular videos')
+    plt.title('Cumulative Redirects For Serving Videos\nVantage Point: %s (Run %d)' % (name, r))
+    plt.xlabel('Datetime')
+    plt.ylabel('Cumulated number of redirects')
+    # ax.set_xticks(df_redirects_vantage_point.index)
+    # ax.set_xticklabels(df_redirects_vantage_point.Hour, rotation=90)
+    # plt.yticks(int)
+    plt.show()
+
+def plot_cumulative_redirects_test(df1, df2, name):
+    for key, value in VANTAGE_POINT_HOSTNAME_LIST.items():  # for name, age in dictionary.iteritems():  (for Python 2.x)
+        if name == value:
+            availability_zone = key
+    figure, axes = plt.subplots(nrows=1, ncols=2)
+    figure.set_size_inches(18.5, 10.5, forward=True)
+    x_ticks = np.arange(0, 4, 1)
+    axes[0].set_xticks(x_ticks)
+    labels = ['12', '18', '0', '6']
+    # ax.set_xticklabels(labels)
+    # ax.locator_params(axis='y', nbins=4)
+    axes[0].set_xticklabels(df1.DateTime, rotation=90)
+    df1.plot(y='CumulRedirectsPopular', use_index=True, kind = 'line', ax=axes[0], label='Popular videos')
+    df1.plot(y='CumulRedirectsNotPopular', use_index=True, kind = 'line', color='red', ax=axes[0], label='Non-popular videos')
+    # plt.title('Cumulative Redirects For Serving Videos\nVantage Point: %s (Run %d)' % (name, r))
+    # axes[0].set_title('Cumulative Redirects For Serving Videos\nVantage Point: %s (Run 1)' % (name))
+    # axes[0].set_xlabel('Datetime')
+    # plt.ylabel('# redirect (cumulative)')
+    figure.text(0.04, 0.5, '# redirect (cumulative)', va='center', rotation='vertical', fontsize=12)
+    # axes[0].set_ylabel('# redirect (cumulative)')
+
+    # axes[1].xticks(x_ticks)
+    # labels = ['12', '18', '0', '6']
+    # ax.set_xticklabels(labels)
+    # ax.locator_params(axis='y', nbins=4)
+    axes[1].set_xticks(x_ticks)
+    axes[1].set_xticklabels(df2.DateTime, rotation=90)
+    df2.plot(y='CumulRedirectsPopular', use_index=True, kind = 'line', ax=axes[1], label='Popular videos')
+    df2.plot(y='CumulRedirectsNotPopular', use_index=True, kind = 'line', color='red', ax=axes[1], label='Non-popular videos')
+    # axes[1].set_xlabel('Datetime')
+    # axes[1].set_ylabel('# redirect (cumulative)')
+    figure.suptitle('Vantage point: %s' % (availability_zone))
+    axes[1].set_title('Second run')
+    axes[0].set_title('First run')
+    # axes[1].xlabel('Datetime')
+    # axes[1].ylabel('Cumulated number of redirects')
+    custom_ylim = (0, 60)
+    plt.setp(axes, ylim=custom_ylim)
+    plt.savefig('/figures/' + availability_zone + '_cumulative_redirect.png')
+    plt.show()
+
 #### MAIN
 
 # zips_orig-folder = 'new/'
@@ -96,6 +204,7 @@ df_ap_northeast_1_by_url.drop(['RedirectUrl', 'StatusCode'], axis=1, inplace=Tru
 
 # create empty total records dataframe
 df_total = pd.DataFrame(columns=DB_COLUMNS)
+df_total_by_url = pd.DataFrame(columns=DB_COLUMNS)
 # create empty vantage point dataframes for totals
 df_us_west_1 = pd.DataFrame(columns=DB_COLUMNS)
 df_us_west_1.drop(['RedirectUrl', 'StatusCode'], axis=1, inplace=True)
@@ -206,6 +315,9 @@ df_uploaded = pd.concat([df_uploaded, subdf], axis=0, ignore_index=True)
 
 df_uploaded['VantagePoint'] = vantage_point
 
+# pd.set_option('display.max_rows', None)
+pd.set_option('display.max_colwidth', -1)
+
 # sorting by ID and reset index
 df_us_west_1_by_url.sort_values('ID', inplace=True)
 df_us_west_1_by_url.reset_index(inplace=True, drop=True)
@@ -219,16 +331,57 @@ df_ap_northeast_1_by_url.sort_values('ID', inplace=True)
 df_ap_northeast_1_by_url.reset_index(inplace=True, drop=True)
 df_uploaded.sort_values('ID', inplace=True)
 df_uploaded.reset_index(inplace=True, drop=True)
-# print(df_us_west_1_by_url)
-# print(df_us_east_1_by_url)
-# print(df_eu_central_1_by_url)
-# print(df_ap_south_1_by_url)
-# print(df_ap_northeast_1_by_url)
-# print(df_uploaded)
+print(df_us_west_1_by_url)
+print(df_us_east_1_by_url)
+print(df_eu_central_1_by_url)
+print(df_ap_south_1_by_url)
+print(df_ap_northeast_1_by_url)
+print(df_uploaded)
+
+
+### Look for similarieties Url <-> IP from different vantage points (NOTE: video uploaded is not included)
+# add vantage point byurl dataframes into total records (groupedbyurl) dataframe
+vantage_point = []
+df_total_by_url = pd.concat([df_total_by_url, df_us_west_1_by_url], axis = 0, ignore_index=True)
+vantage_point.extend([VANTAGE_POINT_HOSTNAME_LIST['us-west-1']]*len(df_us_west_1_by_url.index))
+df_total_by_url = pd.concat([df_total_by_url, df_us_east_1_by_url], axis = 0, ignore_index=True)
+vantage_point.extend([VANTAGE_POINT_HOSTNAME_LIST['us-east-1']]*len(df_us_east_1_by_url.index))
+df_total_by_url = pd.concat([df_total_by_url, df_eu_central_1_by_url], axis = 0, ignore_index=True)
+vantage_point.extend([VANTAGE_POINT_HOSTNAME_LIST['eu-central-1']]*len(df_eu_central_1_by_url.index))
+df_total_by_url = pd.concat([df_total_by_url, df_ap_south_1_by_url], axis = 0, ignore_index=True)
+vantage_point.extend([VANTAGE_POINT_HOSTNAME_LIST['ap-south-1']]*len(df_ap_south_1_by_url.index))
+df_total_by_url = pd.concat([df_total_by_url, df_ap_northeast_1_by_url], axis = 0, ignore_index=True)
+vantage_point.extend([VANTAGE_POINT_HOSTNAME_LIST['ap-northeast-1']]*len(df_ap_northeast_1_by_url.index))
+df_total_by_url['VantagePoint'] = vantage_point
+df_total_by_url.drop(['RedirectUrl', 'StatusCode'], axis=1, inplace=True)
+# groupby url
+df_total_by_url_grouped = df_total_by_url.groupby('Url')
+print("Table for similarieties Url <-> IP lookup between different vantage points")
+for name, group in df_total_by_url_grouped:
+    print(name)
+    print(group[['ID', 'Url', 'IP', 'VantagePoint']])
+    print("\n")
+
+
+### Cumulative redirect comparison between popular and non-popular videos, by vantage point
+popular_video_list = []
+with open('popular_video_urls.txt', 'rt') as f:
+    for line in f.read().splitlines():
+        popular_video_list.append(line)
+not_popular_video_list = []
+with open('not_popular_video_urls.txt', 'rt') as f:
+    for line in f.read().splitlines():
+        not_popular_video_list.append(line)
+df_total_by_url_by_vantage_point = df_total_by_url.groupby('VantagePoint')
+for name, group in df_total_by_url_by_vantage_point:
+    group_run_1, group_run_2 = divide_runs(group)
+    df_redirects_vantage_point_run_1 = cumulative_redirect_stats(name, group_run_1, popular_video_list, not_popular_video_list)
+    # plot_cumulative_redirects(df_redirects_vantage_point_run_1, name, 1)
+    df_redirects_vantage_point_run_2 = cumulative_redirect_stats(name, group_run_2, popular_video_list, not_popular_video_list)
+    # plot_cumulative_redirects(df_redirects_vantage_point_run_2, name, 2)
+    plot_cumulative_redirects_test(df_redirects_vantage_point_run_1, df_redirects_vantage_point_run_2, name)
 
 # sort('Url', 'ID')
-pd.set_option('display.max_rows', None)
-pd.set_option('display.max_colwidth', -1)
 # print(df_us_west_1_by_url.sort_values(['Url', 'ID']))
 
 # print(df_us_west_1_by_url[df_us_west_1_by_url['Url'] == 'https://youtu.be/3YxLYPAoUYQ'][['CacheUrl', 'CacheServerDelay', 'IP', 'PingMin', 'TimeTogetFirstByte']])
@@ -311,5 +464,5 @@ for name, group in grouped_by_vantagepoint_by_cacheurl_test:
 
 # print(df_total)
 
-print(df_ping_table.sort_values('CounterFirstHop', ascending=False))
+# print(df_ping_table.sort_values('CounterFirstHop', ascending=False))
 df_ping_table.dropna(axis=0, how='any', inplace=True)
